abf9c6f5f83607809b4e389a155fb12ce2c18630
王子鑫 <wzix758511990@qq.com>
2021-02-19 20:43:02 +0800

    feat: 个人主页 单元测试

---
+++ src/test/blog/profile.test.js
@@ -0,0 +1,19 @@
+/**
+ * @description 个人主页test
+ * @author wzx
+ */
+
+const { COOKIE, USER_NAME } = require('../testUserInfo')
+const server = require('../server')
+
+test('个人主页加载第一页数据， 应该成功', async () => {
+  const res = await server.get(`/api/profile/loadMore/${USER_NAME}/0`).set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
--- src/test/testUserInfo.js
+++ src/test/testUserInfo.js
@@ -8,5 +8,6 @@
  */
 
 module.exports = {
-  COOKIE: 'weibo.sid=xvtKNKp_u5_ZBtvpGW8SHfPL642-wexZ; weibo.sid.sig=AsmgyX90cUqaqlMR3H2ZbWLtxaI'
+  USER_NAME: 'zhangsan',
+  COOKIE: 'weibo.sid=Ks98bFqZ7NCmBioU6Lqz4tZrOcjs4GRh; weibo.sid.sig=JxloDazs-Nr6ofzUw-8LMeQwfC0'
 }
