91c2e92a94655afc3c664b472898d1a453a6bbb3
王子鑫 <wzix758511990@qq.com>
2021-02-20 09:02:48 +0800

    feat: 广场页面 测试单元

---
+++ src/test/blog/square.test.js
@@ -0,0 +1,19 @@
+/**
+ * @description 广场test
+ * @author wzx
+ */
+
+const server = require('../server')
+const { COOKIE } = require('../testUserInfo')
+
+// 加载第一页数据
+test('广场，加载第一页数据', async () => {
+  const res = await server.get('/api/square/loadMore/0').set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
--- src/test/testUserInfo.js
+++ src/test/testUserInfo.js
@@ -9,5 +9,5 @@
 
 module.exports = {
   USER_NAME: 'zhangsan',
-  COOKIE: 'weibo.sid=Ks98bFqZ7NCmBioU6Lqz4tZrOcjs4GRh; weibo.sid.sig=JxloDazs-Nr6ofzUw-8LMeQwfC0'
+  COOKIE: 'weibo.sid=5xaZfvarCgAJX5WETEfCYS7JRmCNRfIZ; weibo.sid.sig=zdPZMbKxrUI-ukWqZl2vsPWa_eA'
 }
